12b184e46aef29de8a05c6a035562c1bde245a75
DietJoy <96573392+DietJoy@users.noreply.github.com>
2022-04-08 10:36:07 +0200

    etape  OK

--- front/js/script.js
+++ front/js/script.js
@@ -2,21 +2,25 @@
 fetch('http://localhost:3000/api/products')
 .then((response) => response.json())
 .then((data) => {
+console.log(data)
 
 //Affichage des produits avec une boucle for
-for (let i = 0; i < data.lenght; i += 1) {
-    //Déclaration des variables + affectation
+for (let i = 0; i < data.length; i += 1) {
+    //Déclaration des variables + affectation + création des éléments
     let product = document.createElement('article');
     let link = document.createElement('a');
     let image = document.createElement('img');
     let title = document.createElement('h3');
     let description = document.createElement('p');
 
-    document.querrySelector('.items').appendChild(link);
+    document.getElementById('items').appendChild(link); 
 
-        link.href = front/html/product.html?id=$:{data,[i]:_id};
-        link.appendChild(product);
+        link.href = './product.html?id=' + data.id  // Lien vers le produit 
 
+     // Création de l'enfant Product qui a pour parent Link (a)   
+    link.appendChild(product);
+
+    //Création des enfants : Image, Title et Description qui ont pour parent: Product (article)
     product.appendChild(image);
     image.src = data[i].imageUrl;
     image.alt = data[i].altTxt;
@@ -34,4 +38,6 @@ for (let i = 0; i < data.lenght; i += 1) {
 //Ouverture d'une fenêtre de message en cas d'erreur
 .catch(function (error) {
     alert('Le serveur ne répond pas, si le problème persiste, contactez : support@name.com');
+     console.log(error)
   });
+ 
