947cfa5920aae04afe5765535d07236a026048ac
DietJoy <96573392+DietJoy@users.noreply.github.com>
2022-04-11 16:03:20 +0200

    ebauche étape 6 non fonctionelle

--- front/js/product.js
+++ front/js/product.js
@@ -4,7 +4,7 @@
 fetch('http://localhost:3000/api/products')
 .then((response) => response.json())
 .then((data) => {
-console.log(data)
+  console.log(data)
 
 //Création de la variable idProduct qui représente chaque canapé
 //Recupération des paramètres URL avec la propriété window.location.href
@@ -19,6 +19,34 @@ let idProduct = new URL(window.location.href).searchParams.get('id');
 
   let myProduct = findProduct();
 
+/* Etape 6 :  Insérer un produit et ses détails dans la page Produit */
+
+//Création des variables pour l'affichage
+let seeProduct = () => {
+  let productImg = document.createElement("img");
+  let productName = document.getElementsByName("title");
+  let title = document.querySelector('#title');
+  let price = document.querySelector('#price');
+  let description = document.querySelector('#description');
+  let colors = document.querySelector('#colors');
+
+//Affichage du produit
+document.querySelector(".item__img").appendChild(productImg);
+productImg.setAttribute("src", `${myProduct.imageUrl}`);
+productImg.setAttribute("alt", `${myProduct.altTxt}`);
+
+productName[0].textContent = myProduct.name
+title.textContent = myProduct.name
+price.textContent = myProduct.price
+description.textContent = myProduct.description
+
+//Affichage des couleurs par Sélection avec une boucle for in 
+for (let i in myProduct.colors) {
+  colors.insertAdjacentHTML( 'beforeend', `<option value="${myProduct.colors[i]}">${myProduct.colors[i]}</option>`);
+};
+
+};
+seeProduct(); 
 
 }) // Fermeture de fetch
 //Ouverture d'une fenêtre de message en cas d'erreur
